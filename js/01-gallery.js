8b3aab4b043eedcc9895a32d872896bd8e150074
IraGubish2022 <iragubish@gmail.com>
2022-11-11 14:42:00 +0200

    hw-7/1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const galleryEl = document.querySelector('.gallery__item');
-const makeGalleryItems = (items) => {
-    return items
-    .map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryItems = galleryItems
+    .map(({ preview, original, description }) => 
+         `<div class="gallery__item">
         <a class="gallery__link" href='${original}'>
           <img class="gallery__image"
             src='${preview}'
             data-source='${original}'
             alt='${description}'/>
         </a>
-      </div>`;
-    })
+      </div>`)
     .join("");
-}
-galleryEl.insertAdjacentHTML("afterbegin", makeGalleryItems(galleryItems));
 
-const onClick = (Event) => {
-    Event.preventDefault();
-    if (!Event.target.classList.contains('.gallery__image')) 
-    return;
-
-
+galleryEl.insertAdjacentHTML("afterbegin", makeGalleryItems);
 galleryEl.addEventListener('click', onClick);
 
+function onClick (Event) {
+    Event.preventDefault();
+    if (!Event.target.classList.contains('.gallery__image')) {
+    return;
+}
 
-const instance = basicLightbox.create(`<div class = "modal"><img src='${Event.target.dataset.source}'></div>`,
-{
+//const onClick = (Event) => {
+    //Event.preventDefault();
+    //if (!Event.target.classList.contains('.gallery__image')) 
+    //return;
+
+//galleryEl.addEventListener('click', onClick);
+
+const instance = basicLightbox.create(`<div class = "modal"><img src='${Event.target.dataset.source}'></div>`, {
     onOpen: (instance) => {
         window.addEventListener('keydown', onKeyboardClick);
         console.log('onOpen', instance);
@@ -37,14 +41,15 @@ const instance = basicLightbox.create(`<div class = "modal"><img src='${Event.ta
         console.log('onClose', instance);
     }
 });
+
 instance.show()
 
 function onKeyboardClick (Event) {
     if (Event.code === 'Escape') {
         instance.close();
-    }
+    };
 };
 };
-console.log(galleryItems);
+//console.log(galleryItems);
 
 
